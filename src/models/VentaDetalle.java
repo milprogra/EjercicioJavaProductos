25b71ae5f38ec65cbc20d1e996f18480af594618
Matias Jara <mil.programadores.osorno@gmail.com>
2018-11-23 11:40:43 -0300

    Clase VentaDetalle(atributo, constructo,get/set)

---
+++ src/models/VentaDetalle.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author mjara
+ */
+public class VentaDetalle {
+    
+    private int id;
+    private Producto producto;
+    private int cantidad;
+    private int subtotal;
+
+    public VentaDetalle() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(int subtotal) {
+        this.subtotal = subtotal;
+    }
+    
+    
+}
