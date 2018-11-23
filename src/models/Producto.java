8751e9d6c6957d43af19439d94612488bbcaf502
Matias Jara <mil.programadores.osorno@gmail.com>
2018-11-23 11:37:01 -0300

    Clase Producto(atributo, constructo, get/set)

---
+++ src/models/Producto.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author mjara
+ */
+public class Producto {
+    
+    private int id;
+    private String nombre;
+    private int precio;
+    private int stock;
+
+    public Producto() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+    
+}
